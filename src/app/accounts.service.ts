69c6563456bf8c7520301354adf35504a11c488b
RAFAEL SOUZA DA SILVA <rafaelss@ciasc.sc.gov.br>
2017-07-07 12:09:11 -0300

    Aula 101: Injetando serviços em serviços

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,3 +1,9 @@
+import { LoggingService } from "app/logging.service";
+import { Injectable } from "@angular/core";
+
+// Esta anotação deve ser usada apenas no local onde
+// queremos injetar algo
+@Injectable()
 export class AccountsService {
     accounts = [
     {
@@ -14,11 +20,15 @@ export class AccountsService {
     }
   ];
 
+  constructor(private loggingService: LoggingService){}
+
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
